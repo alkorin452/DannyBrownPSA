7fc10f55ec69f59b599cd7f4ed1846898da4053f
alkorin452 <alkorin452@gmail.com>
2017-01-25 15:08:27 -0500

    Updated the script to work with PRAW 4.3

--- DannyBrown.py
+++ DannyBrown.py
@@ -1,32 +1,51 @@
 import praw
 import smtplib
+import time
+
+print("Enter your Reddit username: ")
+global user_name
+user_name = raw_input()
+
+print("Enter your Reddit  password: ")
+global user_password
+user_password = raw_input()
+
+print("Enter your email address: ")
+global email
+email = raw_input()
+
+print("Enter your email password: ")
+global email_password
+email_password = raw_input()
 
 def getDanny():
-
     danny = 'Danny Brown'
     #Enter your reddit usernamer
-    r = praw.Reddit(user_agent = '')
-    submissions = r.get_subreddit('hiphopheads').get_hot(limit = 20)
-    list = [str(x) for x in submissions]
-    for i in list:
-        if danny in i:
+    reddit = praw.Reddit(client_id='iK-oWv7s_BCUNA',
+            client_secret='DG7SqGEZlzd4W_y3UjNS588ey5Y',
+            password=user_password,
+            user_agent='testscript',
+            username=user_name)
+    for submission in reddit.subreddit('hiphopheads').hot(limit = 20):
+        print(submission.title)
+        if danny in submission.title:
             sendEmail()
 
 
 def sendEmail():
 
-    content = 'DANNY BROWN IS ON THE FRONTPAGE OF HHH'
+    content = 'Danny Brown is on the front page of /r/HipHopHeads !'
     mail = smtplib.SMTP('smtp.gmail.com',587)
     mail.ehlo()
     mail.starttls()
     #Email , Password
-    mail.login('','')
+    mail.login(email, email_password) 
     #From email address, Receiver's email, content
-    mail.sendmail('','',content)
+    mail.sendmail(email, email,content)
     mail.close()
 
-
-
-
-getDanny()
+while(True):
+    getDanny()
+    time.sleep(3600)
+   
 
